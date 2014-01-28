82e5b6743bc78d9914b84975a8eaac615cc70bcc
agro <agro@jantox.com>
2014-01-28 13:21:00 -0600

    Fixes #1.

--- src/main/scala/com/gramplr/pile/core/RootService.scala
+++ src/main/scala/com/gramplr/pile/core/RootService.scala
@@ -5,16 +5,31 @@ import spray.http._
 import HttpMethods._
 import spray.can.Http
 import com.gramplr.pile.db.Database
+import com.gramplr.pile.utils.Keygen
+import spray.http.HttpHeaders.Location
 
-class RootService extends Actor with Database {
+class RootService extends Actor with Database with Config {
+
+  lazy val baseurl = getString("url")
 
   def receive = {
     case _: Http.Connected => sender ! Http.Register(self)
     case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
-      sender ! HttpResponse(entity = "welcome to the gramplr pile service")
-    case HttpRequest(GET, Uri.Path("/shorten"), _, _, _) => {
-      sender ! HttpResponse(entity = "http://shorten.gramplr.com/")
-      insertShorten("abcdefg", "thisisrealurl")
+      sender ! HttpResponse(entity = "welcome to the pile service")
+    case r@HttpRequest(GET, Uri.Path("/shorten"), _, _, _) => {
+      val s = r.uri.query.getOrElse("url", "")
+      val key = Keygen.generate
+
+      insertShorten(key, s)
+      sender ! HttpResponse(entity = baseurl + "/" + key)
+    }
+    case HttpRequest(GET, Uri.Path(path), _, _, _) => {
+      val url = getURL(path.substring(1))
+
+      sender ! HttpResponse(
+        status = StatusCodes.TemporaryRedirect,
+        headers = Location(url) :: Nil
+      )
     }
   }
 
--- src/main/scala/com/gramplr/pile/db/Database.scala
+++ src/main/scala/com/gramplr/pile/db/Database.scala
@@ -9,8 +9,22 @@ trait Database extends Config with Core {
   lazy val dbconn = DriverManager.getConnection(getString("db"), getString("user"), getString("password"))
   lazy val table = "url-shorten"
 
+  lazy val insert = dbconn.prepareStatement("INSERT INTO `" + table + "` VALUES (?, ?)")
+  lazy val retrieve = dbconn.prepareStatement("SELECT * FROM `" + table + "` WHERE `key`=?")
+
   def insertShorten(key: String, value: String) {
-    dbconn.prepareStatement("INSERT INTO `" + table + "` VALUES (\"" + key + "\", \"" + value + "\")").executeUpdate()
+    insert.setString(1, key)
+    insert.setString(2, value)
+    insert.executeUpdate()
+  }
+
+  def getURL(key: String): String = {
+    retrieve.setString(1, key)
+    val rs = retrieve.executeQuery()
+    if(rs.next())
+      rs.getString("value")
+    else
+      "shorten.gramplr.com"
   }
 
 }
